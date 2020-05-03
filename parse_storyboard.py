f1ad1e86bc92d3484a8b325cc238b9eef0319101
Rudi Farkas <rudi.farkas@gmail.com>
2020-05-03 15:24:51 +0200

    clean up code and comments

--- parse_storyboard.py
+++ parse_storyboard.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+# references
 # https://www.datacamp.com/community/tutorials/python-xml-elementtree
 # https://joris.kluivers.nl/blog/2014/02/10/storyboard-identifier-constants/
+# https://graphviz.readthedocs.io/en/stable/index.html
 
 import sys, os
 import xml.etree.ElementTree as et
@@ -14,7 +16,7 @@ class StoryboardParser(object):
 
     def __init__(self, filepath):
         """Given a filepath to an Xcode .storyboatd file
-        initialize the instance parsing the file.
+        initialize the instance and parse the file.
         """
         self.filepath = filepath
         self.dir = re.split(r'\/', filepath)[0]
@@ -26,13 +28,16 @@ class StoryboardParser(object):
         self.controller_name_dict = {node['id']: node['controller_name'] for node in self.controller_nodes}
 
         self.segue_edges = self.collect_segue_edges()
-        self.unwind_edges = [] #self.collect_unwind_edges()
 
     ##########################
 
     def digraph(self):
+        """
+        From data collected from the input storyboard file construct a 
+        graphviz dot object (textual representatation of graph of viewControllers and segues)
+        and render it as .png
+        """
         from graphviz import Digraph, Source
-        # https://graphviz.readthedocs.io/en/stable/api.html
 
         dot = Digraph(name='storyboard')
         dot.attr('node', shape='rect', fontname = 'courier', fontsize = '12')
@@ -55,11 +60,17 @@ class StoryboardParser(object):
         dot.render('graphviz-out/graphviz', view=True)
         return dot.source
 
+    ### collect data from storyboard
+
+    def initial_vc_class_name(self):
+        initial_vc_id = self.root.attrib['initialViewController']
+        vc_class_name_dict = {vc_node['id']: vc_node['controller_name'] for vc_node in self.controller_nodes}
+        return vc_class_name_dict.get(initial_vc_id, 'UNKNOWN')
+
     def collect_controller_nodes(self):
         """
         Return nodes representing viewControllers and navigationControllers
         """
-
         def vc_node(vc):
             return { 'id': vc.attrib['id'],
                     'controller_name': vc.attrib['customClass']}
@@ -74,11 +85,6 @@ class StoryboardParser(object):
         for nc in self.root.iter('navigationController'):
             nodes.append(navc_node(nc))
         return nodes
-
-    def initial_vc_class_name(self):
-        initial_vc_id = self.root.attrib['initialViewController']
-        vc_class_name_dict = {vc_node['id']: vc_node['controller_name'] for vc_node in self.controller_nodes}
-        return vc_class_name_dict.get(initial_vc_id, 'UNKNOWN')
 
     def collect_segue_edges(self):
         """
@@ -102,7 +108,7 @@ class StoryboardParser(object):
                     'identifier': segue.attrib['identifier']}
 
         def unwind_segue_edge(controller_names, vc_name, segue):
-            identifier = segue.attrib['identifier'] # may in 'VC'
+            identifier = segue.attrib['identifier'] # may end in 'VC'
             identifier_full = identifier.replace('VC', 'controller_name')
             dest_vc_name = find_nearest(identifier_full, controller_names)
             print('***', identifier, dest_vc_name)
@@ -125,68 +131,12 @@ class StoryboardParser(object):
                     edge = presentation_segue_edge(controller_name_dict, vc_name, segue)
                 elif kind == 'unwind':
                     edge = unwind_segue_edge(controller_name_dict.values(), vc_name, segue)
-                    print('segue.attrib:', segue.attrib)
-                    print('edge:', edge)
                 if edge:
                     edge['kind'] = kind
                     edges.append(edge)
         return edges
 
-    def segue_edge(self, controller_names, vc, segue):
-        if segue.attrib['kind'] == 'presentation':
-            dest_vc_id = segue.attrib['destination']
-            dest_vc_class_name = controller_names[dest_vc_id]
-            return { 'id': segue.attrib['id'],
-                    'source': vc.attrib['customClass'],
-                    'destination': dest_vc_class_name,
-                    'identifier': segue.attrib['identifier'],
-            }
-        return None 
-
-    def collect_relationship_segue_edges(self):
-        """
-        Return edges representing segues
-        """
-        # TODO
-
-
-    def relationship_segue_edge(self, vc_class_name_dict, vc, segue):
-        if segue.attrib['kind'] == 'relationship':
-            dest_vc_id = segue.attrib['destination']
-            dest_vc_class_name = vc_class_name_dict[dest_vc_id]
-            return { 'id': segue.attrib['id'],
-                    'source': vc.attrib['customClass'],
-                    'destination': dest_vc_class_name,
-                    'identifier': segue.attrib['identifier'],
-            }
-        return None 
-
-    def collect_unwind_edges(self):
-        """
-        Return edges representing unwind segues
-        """
-        vc_class_names = [vc_node['controller_name'] for vc_node in self.controller_nodes]
-        edges = []
-        for vc in self.root.iter('controller_name'):
-            for segue in vc.iter('segue'):
-                edge = self.unwind_edge(vc_class_names, vc, segue)
-                if (edge):
-                    edges.append(edge)
-        return edges
-
-    def unwind_edge(self, vc_class_names, vc, segue):
-        if (segue.attrib['kind'] == 'unwind'):
-            identifier = segue.attrib['identifier'] # ends in 'VC'
-            identifier_full = identifier.replace('VC', 'controller_name')
-            dest_vc_class_name = find_nearest(identifier_full, vc_class_names)
-            return { 'id': segue.attrib['id'],
-                    'source': vc.attrib['customClass'],
-                    'destination': dest_vc_class_name,
-                    'identifier': identifier,
-            }
-        return None 
-
-    ### preliminary investigation
+    ### for preliminary investigation
 
     def root_info(self):
         strs = []
@@ -201,7 +151,6 @@ class StoryboardParser(object):
             strs.append(json.dumps(vc.attrib))
             #strs.append(self.dump(vc.attrib, ['id']))
             for segue in vc.iter('segue'):
-                #     print(segue)
                 strs.append('        segue ' + self.dump(segue.attrib, ['id', 'destination', 'kind', 'identifier', 'unwindAction', 'modalPresentationStyle',  'modalTransitionStyle']))
                 #{"destination": "aZf-qq-fub", "kind": "presentation", "identifier": "toStickerShopVC", "modalPresentationStyle": "fullScreen", "modalTransitionStyle": "crossDissolve", "id": "Dde-ZG-iRW"}
         strs.append('')
@@ -228,32 +177,6 @@ class StoryboardParser(object):
         #return "\n".join(strs)
         return ''
 
-    def nodes_and_edges(self):
-        """
-        Returns nodes (view controllers) and edges (segues)
-        """
-        nodes = []
-        edges = []
-        for vc in self.root.iter('controller_name'):
-            nodes.append(self.any_vc_node(vc))
-            for segue in vc.iter('segue'):
-                edges.append(self.any_segue_edge(vc, segue))
-        return {'nodes': nodes, 'edges': edges}
-
-    def any_vc_node(self, vc):
-        node = { 'id': vc.attrib['id'],
-                'controller_name': vc.attrib['customClass']
-        }
-        return node
-
-    def any_segue_edge(self, vc, segue):
-        edge = { 'id': segue.attrib['id'],
-                'source': vc.attrib['id'],
-                'destination': segue.attrib['destination'],
-        }
-        return edge
-
-
     def dump(self, dict, keys):
         strs = []
         for key in keys:
@@ -274,15 +197,6 @@ def print_detail_info(sb):
             print(f'  {key}: {value}')   
 
     print('root_info:', sb.root_info())
-    # print('navigationControllers_info:', sb.navigationControllers_info())
-    # print('viewControllers_info:', sb.viewControllers_info())
-    # print('segue_info:', sb.segue_info())
-
-    # ne = sb.nodes_and_edges()
-
-    # print_list('nodes', ne['nodes'])
-    # print_list('edges', ne['edges'])
-
     print_list('controller_nodes', sb.controller_nodes)
     print_dict('controller_name_dict', sb.controller_name_dict)
     print_list('segue_edges', sb.segue_edges)
@@ -295,7 +209,7 @@ if __name__ == "__main__":
 
     sb = StoryboardParser(args.filepath)
     print_detail_info(sb)
-    ds = sb.digraph()
-    print(ds)
+    sbd = sb.digraph()
+    print(sbd)
 
 
